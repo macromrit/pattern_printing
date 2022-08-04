5a940f8234ad042292b7c813f63869f799abb15e
macromrit <77322873+macromrit@users.noreply.github.com>
2022-08-04 22:50:42 +0530

    Update spatial_diamonds.py

--- spatial_diamonds.py
+++ spatial_diamonds.py
@@ -1,4 +1,14 @@
-def pattern_func(cnt):
+def pattern_func(cnt: int, chr: str)->None:
+    """to print spatial diamonds via the parametrs given by user
+
+    Args:
+        cnt (int): the max length of print to be displayed 
+        chr (str): the character that should be used to display pattern
+
+    Raises:
+        Exception: cnt's val should be >= 9 and should be an odd number
+    """
+    
     if cnt%2 and cnt>=9:
         # +2 for every diamond space layer
         #max space char to be manufactured -> cnt-2
@@ -12,10 +22,10 @@ def pattern_func(cnt):
         while max<=cnt-2:
             #body
             if max==0:
-                print('*'*cnt)
+                print(chr*cnt)
             else: 
                 cal = (cnt-max)//2
-                print('*' * cal, ' ' * max, '*' * cal, sep='')
+                print(chr * cal, ' ' * max, chr * cal, sep='')
             
             #update statement
             if max:
@@ -29,13 +39,13 @@ def pattern_func(cnt):
         while ref>=0:
             #body
             space_cnt = (cnt-ref)//2
-            print('*'*space_cnt, ' '* ref,'*'*space_cnt, sep='') 
+            print(chr*space_cnt, ' '* ref, chr*space_cnt, sep='') 
              
             #update statement        
             ref-=2
             
             if ref==-1:
-                print('*'*cnt)
+                print(chr*cnt)
                 
         
                 
