97e09f16be2d60072e7c56c2b3b10b0df2b6dda9
macromrit <77322873+macromrit@users.noreply.github.com>
2022-08-05 19:03:28 +0530

    Create reverse_triangle_print.py

---
+++ reverse_triangle_print.py
@@ -0,0 +1,10 @@
+def revr_triangle_print(cnt: int, char: str)->int:
+    if cnt==0:
+        return 
+    else:
+        print(char*cnt)
+        revr_triangle_print(cnt-1, char)
+
+
+if __name__ == "__main__":
+    pass
