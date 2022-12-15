62f323583c1ddd3d3c4d2ef850fdc9a9b6772813
macromrit <77322873+macromrit@users.noreply.github.com>
2022-12-15 22:48:55 +0530

    Update pyramid_via_recursion.py

--- pyramid_via_recursion.py
+++ pyramid_via_recursion.py
@@ -1,1 +1,21 @@
+#pyramid pattern printin 
+#expected output -> 
+#print_pyrmd(5)->
+'''
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+'''
 
+def print_pyrmd(n: int, cnt: int = 1)->None:
+    if cnt==n:
+        print(" "*0+"* "*n)
+        return
+    else:
+        print(" "*(n-cnt)+"* "*cnt)
+        print_pyrmd(n, cnt+1)
+
+if __name__=="__main__":
+  pass
